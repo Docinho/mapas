5ca760e5c839d3e7f02ba3b9db85b372fb01ca0a
docinho <livia.juliao@ccc.ufcg.edu.br>
2018-02-25 23:57:46 -0300

    ajustando os arquivos do ultimo lab

---
+++ lab3/commas.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+# (C) Júlio Barreto
+# Replace commas with dots
+
+data = open("result.csv", 'r')
+lines = data.readlines()
+
+for i in range(len(lines)):
+	lines[i] = lines[i].replace(";",",")
+
+to_write = "".join(lines)
+
+with open("result.csv",'w') as result:
+	result.write(to_write)
+
+data.close()
